b16ed0b9e5b0d9035dc688ab9b19f089b26b8bca
terrnit <terrnit.megaman@gmail.com>
2022-07-17 22:46:47 +0500

    complete script / fix wrong api fetch-response

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ weather.temperature = {
 };
 
 const KELVIN = 273;
-const KEY = "82005d27a116c2880c8f0fcb866998a0";
+const key = "1d77cb7bd321a3b974d663238a45e24d";
 
 // Browser support geolocation?
 
@@ -41,3 +41,55 @@ function showError(error) {
   notificationElement.style.display = "block";
   notificationElement.innerHTML = `<p>${error.message}</p>`;
 }
+
+// Get weather for api provider
+
+function getWeather(latitude, longitude) {
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+  fetch(api)
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
+      weather.city = data.name;
+      weather.country = data.sys.country;
+    })
+    .then(function () {
+      displayWeather();
+    });
+}
+
+// Display weather to UI
+
+function displayWeather() {
+  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png">`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span></span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F
+
+function celsiusToFahrenheit(temperature) {
+  return (temperature * 9) / 5 + 32;
+}
+
+// Click on the temperature element
+
+tempElement.addEventListener("click", function () {
+  if (weather.temperature.value === undefined) return;
+
+  if (weather.temperature.init == "celsius") {
+    let fahrenheit = Math.floor(fahrenheit);
+
+    tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+    weather.temperature.init = "fahrenheit";
+  } else {
+    weather.temperature.init = "celsius";
+  }
+});
